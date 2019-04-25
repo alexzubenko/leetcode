ae25430b3c47b091f5543e86b84b0bde4aa00033
Oleksandr Zubenko <Oleksandr_Zubenko@epam.com>
2019-04-24 17:41:24 -0700

    adding solution for count prime numbers, half of it i got, need to do debug it again on paper

--- math/count_primes.py
+++ math/count_primes.py
@@ -12,22 +12,79 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 
 class Solution(object):
-    # this one works, but it is too slow
     def countPrimes(self, n):
-        count = 0
-        for i in range(n):
-            if i > 1:
-                check = 1
-                for j in range(2, i):
-                    if (i % j) == 0:
-                        print(i, "is not a prime number")
-                        print(j, "times", i // j, "is", i)
-                        check = 0
-                        break
-                if check!=0:
-                    count+=1
+        if n <= 2:
+            return 0
+        # Creating the list which will help us to count prime numbers
+        # we can populate it with 1,1,1,1,1
+        # or True, True, True, ...
+        res = [1] * n
+        print(res)
+        # first two numbers from range (0 and 1) are not the prime numbers
+        # and we will not include them in the further calculations
+        res[0] = res[1] = 0
+        print(res)
+        for i in range(2, n):
+            print('i: ',i)
+            # here we are checking only the numbers where
+            # in the check list we have 1
+            if res[i] == 1:
+                print('here we will check the numbers in range('
+                      '{}, ({}-1)/ {} + 1), which is equal {}/{}'.format(
+                    i,n,i,n-1,i+1
+                ))
+                for j in range(i, (n - 1) // i + 1):
+                    print('j: ', j)
+                    res[i * j] = 0
+                    print('we will zero the following element in check array',i*j)
+                    print(res)
+        return sum(res)
 
-        return count
+solution = Solution()
+print(solution.countPrimes(10))
+    # another version which works but too long for leetcode
+    # def countPrimes(self, n):
+    #     if n < 2:
+    #         return 0
+    #     count = [1] * n
+    #     print(count)
+    #     count[0] = count[1] = 0
+    #     print(count)
+    #     for i in range(n):
+    #         print(i)
+    #         if count[i] == 1:
+    #             check = 1
+    #             for j in range(2, i):
+    #                 if (i % j) == 0:
+    #                     print(i, "is not a prime number")
+    #                     print(j, "times", i // j, "is", i)
+    #                     check = 0
+    #                     count[i] = 0
+    #                     break
+    #             if check!=0:
+    #                 print('{} is prime number'.format(i))
+    #                 count[i] = 1
+    #                 print(count)
+    #
+    #     return sum(count)
+
+
+    # this one works, but it is too slow
+    # def countPrimes(self, n):
+    #     count = 0
+    #     for i in range(n):
+    #         if i > 1:
+    #             check = 1
+    #             for j in range(2, i):
+    #                 if (i % j) == 0:
+    #                     print(i, "is not a prime number")
+    #                     print(j, "times", i // j, "is", i)
+    #                     check = 0
+    #                     break
+    #             if check!=0:
+    #                 count+=1
+    #
+    #     return count
     # def countPrimes(self, n):
     #     count = 0
     #     for i in range(n):
@@ -124,5 +181,3 @@ class Solution(object):
 
 
 
-solution = Solution()
-print(solution.countPrimes(7))
