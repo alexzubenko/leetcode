7cc5e693acfcfea4df6007e651b64b1a5520688e
Oleksandr Zubenko <Oleksandr_Zubenko@epam.com>
2019-09-03 07:31:00 -0700

    add cople of more solutions

--- codeacademy/technical_interview/find_duplicates_in_string.py
+++ codeacademy/technical_interview/find_duplicates_in_string.py
@@ -6,10 +6,34 @@ class Solution:
                     return True
         return False
 
+    def find_dups_instring_has_table(self, stringg):
+        check = {}
+        for i in stringg:
+            if i in check:
+                check[i] = 1
+            else:
+                check[i] = 0
+        for key in check.keys():
+            if check[key] == 1:
+                return True
+        return False
+
+    def find_dups_remove(self, stringg):
+        lenn = len(stringg)
+        while lenn:
+            if stringg[0] in stringg[0+1:]:
+                return True
+            else:
+                stringg = stringg[0+1:]
+                lenn -=1
+        return False
+
 
 
 
 
 sol = Solution()
-print(sol.find_dups_instring('trao'
+print(sol.find_dups_instring('toprt'
 ))
+print(sol.find_dups_instring_has_table('toprt'))
+print(sol.find_dups_remove('toprt'))
